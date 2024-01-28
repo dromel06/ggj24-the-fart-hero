82282790b2a4c2492b028b166c77dfb83de34276
Alejandro-ROG <alejandro.anguizola@gmail.com>
2024-01-28 14:54:47 -0500

    Added missing point awarder

---
+++ Assets/InputAppraisalSystem/Scripts/Monobehaviours/PointAwarder.cs
@@ -0,0 +1,33 @@
+using System;
+using UnityEngine;
+
+public class PointAwarder : MonoBehaviour
+{
+    private float score = 0;
+    private GameInputEvaluator evaluator;
+
+    [SerializeField] private float mistakeScore;
+    [SerializeField] private float goodScore;
+    [SerializeField] private float excelentScore;
+
+
+    public static event Action<float> OnScoreChange;
+
+    private void OnEnable()
+    {
+        evaluator.OnExcellentInputDown += HandleExcelentInput;
+        evaluator.OnGoodInputDown += HandleGoodInput;
+        evaluator.OnBadInputIndexDown += HandleMistakeInput;
+    }
+
+    void HandleExcelentInput() => AddScore(excelentScore);
+    void HandleGoodInput() => AddScore(goodScore);
+    void HandleMistakeInput() => AddScore(mistakeScore);
+
+
+    void AddScore(float points)
+    {
+        score += points;
+        OnScoreChange?.Invoke(score);
+    }
+}
