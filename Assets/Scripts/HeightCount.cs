1f5f7760036624340e824075895240534cce318d
Diomedes Diaz <d.romeldiaz@gmail.com>
2024-01-27 19:34:49 -0500

    radio and efects

--- Assets/Scripts/HeightCount.cs
+++ Assets/Scripts/HeightCount.cs
@@ -8,6 +8,7 @@ public class HeightCount : MonoBehaviour
     public Text heightText;
     public Text heightLb;
     public float height;
+    private string listOfS;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,20 +16,31 @@ public class HeightCount : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
-    {
+    void Update(){
         if(Input.GetKeyDown(KeyCode.UpArrow)){
-            addHeight(0.01f);
+            addHeight(0.5f);
         }
         if(Input.GetKeyDown(KeyCode.DownArrow)){
-            removeHeight(0.01f);
+            removeHeight(0.5f);
         }
 
         heightText.text = height.ToString("0.00");
-        if(height > 0){
-            heightLb.text = "<LBS>";
-        }else{
+        if (height < 1){
             heightLb.text = "<>";
+        }
+        else if(height >= 1 && height < 2){
+            heightLb.text = "<S>";
+        }else if(height >= 2 && height < 3){
+            heightLb.text = "<BS>";
+        }else if(height >= 3 && height < 6){
+            heightLb.text = "<LBS>";
+        }
+        else{
+            listOfS = "";
+            for(int i = 0; i < height-6; i+= 2){
+                listOfS += "S";
+            }
+            heightLb.text = "<LB" + listOfS + ">";
         }
     }
 
